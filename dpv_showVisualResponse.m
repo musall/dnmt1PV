12687e1de384ed0107efc2523f3960aac561ab85
Simon Musall <49614434+musall@users.noreply.github.com>
2026-02-05 12:19:49 +0100

    Update dpv_showVisualResponse.m

--- dpv_showVisualResponse.m
+++ dpv_showVisualResponse.m
@@ -1,8 +1,3 @@
-%% define experiments to use
-recLabels = {'Animal','ExperimentDate','Location','Expertise','Folder','Probe','Background','Path','Ramppower','useRec','hasGamma'};
-recInfo{1} = {'PV2555','26/01/2022','V1','Naive','PV2555_20221026','0','PV-Cre','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','Control';'PV2555','28/01/2022','V1','Naive','PV2555_20221028','0','PV-Cre','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','Control';'PV2555','29/01/2022','V1','Naive','PV2555_20221029','0','PV-Cre','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','Control';'PV2556','26/01/2022','V1','Naive','PV2556_20221026','0','PV-Cre','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','Control';'PV2556','27/01/2022','V1','Naive','PV2556_20221027','0','PV-Cre','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','Control';'PV2556','28/01/2022','V1','Naive','PV2556_20221028','0','PV-Cre','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','Control';'PV2556','29/01/2022','V1','Naive','PV2556_20221029','0','PV-Cre','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','Control'};
-recInfo{2} = {'PV2632','09/02/2023','V1','Naive','PV2632_20230209','0','PV-DNMT1','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','PV-DNMT1';'PV2632','10/02/2023','V1','Naive','PV2632_20230210','0','PV-DNMT1','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','PV-DNMT1';'PV2632','11/02/2023','V1','Naive','PV2632_20230211','0','PV-DNMT1','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','PV-DNMT1';'PV2633','24/01/2023','V1','Naive','PV2633_20230124','0','PV-DNMT1','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','PV-DNMT1';'PV2633','25/01/2023','V1','Naive','PV2633_20230125','0','PV-DNMT1','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','PV-DNMT1';'PV2633','26/01/2023','V1','Naive','PV2633_20230126','0','PV-DNMT1','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','PV-DNMT1';'PV2633','27/01/2023','V1','Naive','PV2633_20230127','0','PV-DNMT1','\\naskampa.kampa-10g\lts\invivo_ephys\Neuropixels\','high','1','1','PV-DNMT1'};
-
 %% basic variables
 opts.Location = {'S1'};
 opts.makePlot = false;
@@ -18,13 +13,13 @@ opts.brainRange = [-200, 1500]; %depth range for plotting
 opts.verbose = false; %flag to supress some of the text outputs
 opts.brainThresh = 1;
 opts.groupColors = {([212, 212, 212]./255), ([255, 160, 64]./255), [0, 0, 1], [1, 0, 0]};
-opts.optoGenetics = false; %flag to isolate sensory responses during optogenetics
 opts.layerRange = [500, 1000]; %range for computing CSD traces
 opts.layerDepths = 25 : 75 : 1000; %depths of different layers in microns
 ctrlColor = [212 212 212]./255;
 KOcolor = [255 160 64]./255;
-opts.savePath = 'D:\DNMT1_project\';
+opts.savePath = 'F:\DNMT1_project\';
 opts.reload = false;
+opts.imecNr = '0';
 
 %% run over groups
 nrGroups = length(opts.groups);
@@ -35,33 +30,21 @@ allLFP = cell(1, length(opts.groups));
 allStimStd = cell(1, length(opts.groups));
 for iGroups = 1 : nrGroups
     
-    recIdx = strcmpi(recLabels, 'Folder');
-    pathIdx = strcmpi(recLabels, 'Path');
-    allInfo{iGroups} = recInfo{iGroups};
+    cPath = fullfile(opts.savePath, opts.groups{iGroups});
+    recs = dir([cPath filesep '**' filesep 'csdResponse_imec' opts.imecNr '.mat']);
     
-    for iRecs = 1 : size(recInfo{iGroups}, 1)
-        try
-            
-            [csdResp, lfpResp, meanStd, normDepth, lfMeta] = ds_loadCSDresponse(recInfo{iGroups}(iRecs,:), recLabels, opts);
-            recName = recInfo{iGroups}{iRecs,strcmpi(recLabels, 'Folder')};
-            recPath = recInfo{iGroups}{iRecs,strcmpi(recLabels, 'Path')};
+    allCSD{iGroups} = cell(1, length(recs));
+    allLFP{iGroups} = cell(1, length(recs));
+    allStimStd{iGroups} = cell(1, length(recs));
+    for iRecs = 1 : length(recs)
+        
+        saveFile = fullfile(recs(iRecs).folder, recs(iRecs).name);
+        cFile = load(saveFile, 'csdResp', 'lfpResp', 'meanStd', 'normDepth', 'lfMeta');
+        
+        allCSD{iGroups}{iRecs} = cFile.csdResp;
+        allLFP{iGroups}{iRecs} = cFile.lfpResp;
+        allStimStd{iGroups}{iRecs} = cFile.meanStd;
 
-            % keep info in larger array
-            if isempty(allCSD{iGroups})
-                allCSD{iGroups} = {csdResp};
-                allLFP{iGroups} = {lfpResp};
-                allStimStd{iGroups} = {meanStd};
-                usedRecs{iGroups} = {fullfile(recInfo{iGroups}{iRecs,pathIdx}, recInfo{iGroups}{iRecs,recIdx})};
-            else
-                allCSD{iGroups} = [allCSD{iGroups}; csdResp];
-                allLFP{iGroups} = [allLFP{iGroups}; lfpResp];
-                allStimStd{iGroups} = [allStimStd{iGroups}; meanStd];
-                usedRecs{iGroups} = [usedRecs{iGroups}; {fullfile(recInfo{iGroups}{iRecs,pathIdx}, recInfo{iGroups}{iRecs,recIdx})}];
-            end
-        catch ME
-            disp(['Aborted: ' ME.message]);
-            failedRecs{iGroups} = {failedRecs{iGroups}; recInfo{iGroups}{iRecs,recIdx}};
-        end
     end
 end
 
@@ -79,17 +62,18 @@ end
 
 %% combined figures - LFP and CSD for each group
 normStim = cell(1, length(opts.groups));
-cRange = 1.5*1E6; %for tactile
-lfpRange = 0.7; %for vision
-stimLabel = 'Tactile';
-traceRange = [-7 2]; %for CSD
+cRange = 1.5*1E6;
+lfpRange = 0.7;
+stimLabel = 'Visual';
+traceRange = [-3 1]; %for CSD
+lfMeta = cFile.lfMeta;
 
 if opts.stimType == 1
-    lfpRange = 0.7;
-    csdRange = 6*1E5;
+    lfpRange = 0.4;
+    csdRange = 2.5;
 elseif opts.stimType == 3
-    lfpRange = 0.7;
-    csdRange = 1*1E6;
+    lfpRange = 150;
+    csdRange = 1.3*1E6;
 end
 
 clear lines
@@ -97,7 +81,7 @@ depthLabel = {'Supragranular', 'Infragranular'};
 respLabels = {'LFP absolute', 'CSD absolute', 'CSD rescale'};
 h1 = figure('renderer' ,'painters');
 h2 = figure('renderer' ,'painters');
-xRange = [(opts.baseDur - 0.05), (opts.baseDur + 0.1)] .*lfMeta.sRateHz;
+xRange = [(opts.baseDur - 0.05), (opts.baseDur + 0.2)] .*lfMeta.sRateHz;
 baseDur = round(opts.baseDur .*lfMeta.sRateHz);
 nrLayers = length(opts.layerDepths);
 peakResp = cell(nrGroups,2);
@@ -120,13 +104,13 @@ for iGroups = 1 : nrGroups
             mergeStim = mergeStim - nanmean(mergeStim(:, 1:baseDur, :), 2);
 %             cRange = csdRange;
             cRange = prctile(abs(mergeStim(:)),99.5);
-            depthRange = round(normDepth * 1E6);
+            depthRange = round(cFile.normDepth * 1E6);
             
         elseif x == 3
             mergeStim = cat(3,allCSD{iGroups}{:});
             mergeStim = mergeStim - nanmean(mergeStim(:, 1:baseDur, :), 2);
             cRange = prctile(abs(mergeStim(:)),99.7);
-            depthRange = round(normDepth * 1E6);
+            depthRange = round(cFile.normDepth * 1E6);
         end
         
         % show image
@@ -137,10 +121,9 @@ for iGroups = 1 : nrGroups
         xlim(xRange);
         caxis([-cRange cRange]);
         colormap(ax, parula(256));
-        colorbar
         niceFigure;
         
-        title([opts.Location{1} ' - ' opts.groups{iGroups} '; ' stimLabel ' stimulation; ' respLabels{x} '; optogenetics: ' num2str(opts.optoGenetics)]);
+        title([opts.Location{1} ' - ' opts.groups{iGroups} '; ' stimLabel ' stimulation; ' respLabels{x}]);
         xlabel('time(ms)')
         nvline(opts.baseDur*lfMeta.sRateHz, 'w', 'linewidth', 2);
         
@@ -191,10 +174,9 @@ for iGroups = 1 : nrGroups
             colorRange = prctile(abs(mergeStim(:)),99.5);
             caxis([-colorRange colorRange]);
             colormap(ax, parula(256));
-            colorbar
             niceFigure;
             
-            title([opts.Location{1} ' - ' opts.groups{iGroups} '; ' stimLabel ' stimulation; ' respLabels{x} '; optogenetics: ' num2str(opts.optoGenetics)]);
+            title([opts.Location{1} ' - ' opts.groups{iGroups} '; ' stimLabel ' stimulation; ' respLabels{x}]);
             xlabel('time(ms)')
             nvline(opts.baseDur*lfMeta.sRateHz, 'w', 'linewidth', 2);
             
@@ -218,7 +200,7 @@ for iGroups = 1 : nrGroups
                 mergeTrace{iDepth} = mergeTrace{iDepth} - mergeTrace{iDepth}(abs(t) == min(abs(t)),:);
                 lines(iGroups) = stdshade(mergeTrace{iDepth}', 0.5, opts.groupColors{iGroups}, t);
                 title([opts.Location{1} '; ' stimLabel ' stimulation; ' respLabels{x} '; ' depthLabel{iDepth}]);
-                ylabel('LFP deflection (uV)');
+                ylabel('CSD deflection');
                 xlabel('time (s)');
                 niceFigure;
                 cIdx = t>0 & t < 0.1;
